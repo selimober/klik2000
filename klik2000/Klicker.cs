d2244f9e523c9484d498c26182d2514cfc2bca01
Selim Ober <selim.ober@gmail.com>
2011-10-18 13:48:11 +0300

    Initial commit

---
+++ klik2000/Klicker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace klik2000
+{
+    class Klicker
+    {        
+        private Thread clickingthread;
+        private int sleepTime;
+
+        public Klicker(int sleepTime)
+        {
+            this.sleepTime = sleepTime;
+            clickingthread = new Thread(SleepAndClick);
+            clickingthread.Start();
+        }
+
+        private void SleepAndClick()
+        {            
+            Thread.Sleep(sleepTime);
+            DoMouseClick();
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public void DoMouseClick()
+        {
+            try
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            }
+            catch (Exception ex) { showerrormessage(ex); }
+        }
+
+        void showerrormessage(Exception error)
+        {
+            MessageBox.Show("Error:  " + error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+        }
+    }
+}
---
+++ klik2000/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace klik2000
+{
+    static class Program
+    {
+
+        private const int DEFAULT_SLEEP_TIME = 30000;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] Args)
+        {
+            int sleepTime = FindSleepTimeFromArgs(Args);
+            Klicker klicker = new Klicker(sleepTime);
+        }
+
+        private static int FindSleepTimeFromArgs(string[] Args)
+        {
+            if (Args.Length == 0)
+            {
+                return DEFAULT_SLEEP_TIME;
+            }
+            else
+            {
+                int result = 0;
+                try
+                {
+                    result = Convert.ToInt32(Args[0]) * 1000;                    
+                }
+                catch
+                {
+                    result = DEFAULT_SLEEP_TIME;
+                }
+
+                return result;
+            }
+        }        
+    }
+}
